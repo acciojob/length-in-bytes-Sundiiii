00a332155bfd3d489dabb97e5881aa612271d908
Sundar  Deshmukh <109438264+Sundiiii@users.noreply.github.com>
2023-10-18 17:58:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const byteSize = (str) => {
-  // write your code here
-};
+  const str1=str.length;
+	return str1;
+	};
 
 // Do not change the code below
 const str = prompt("Enter some string.");
